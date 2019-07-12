8274632be90572283afc0398387325edc5c8cf3d
davidfu <davidfu1974@163.com>
2019-07-12 20:26:35 +0800

    修改了EXCELHELPER类，采购入库单加入仓库分组、存货编码改为存货名称，入库单备注，委外件数修改

---
+++ BestU8/ExcelHelper.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+
+namespace BestU8
+{
+    public class ExcelHelper 
+    {
+        public ExcelHelper()
+        {
+        }
+
+        public DataTable ReadExcelToDatatble(string worksheetName, string saveAsLocation, int HeaderLine, int ColumnStart)
+        {
+            System.Data.DataTable dataTable = new DataTable();
+            Microsoft.Office.Interop.Excel.Application excel;
+            Microsoft.Office.Interop.Excel.Workbook excelworkBook;
+            Microsoft.Office.Interop.Excel.Worksheet excelSheet;
+            Microsoft.Office.Interop.Excel.Range range;
+            try
+            {
+                // Start Excel and get Application object.
+                excel = new Microsoft.Office.Interop.Excel.Application();
+
+                // for making Excel visible
+                excel.Visible = false;
+                excel.DisplayAlerts = false;
+
+                // Creation a new Workbook
+                excelworkBook = excel.Workbooks.Open(saveAsLocation);
+                
+                // Workk sheet
+                excelSheet = (Microsoft.Office.Interop.Excel.Worksheet)excelworkBook.Worksheets.Item[worksheetName];
+                range = excelSheet.UsedRange;
+                
+                int cl = range.Columns.Count;
+                // loop through each row and add values to our sheet
+                int rowcount = range.Rows.Count; ;
+
+                for (int j = ColumnStart; j <= cl; j++)
+                {
+                    dataTable.Columns.Add(Convert.ToString(range.Cells[HeaderLine, j].Value2), typeof(string));
+                }
+                for (int i = HeaderLine + 1; i <= rowcount; i++)
+                {
+                    DataRow dr = dataTable.NewRow();
+                    for (int j = ColumnStart; j <= cl; j++)
+                    {
+                        //判断是否为日期格式的单元格
+                        string dateformat = range.Cells[i, j].NumberFormat;
+                        if (dateformat.IndexOf("yyyy") == -1)
+                        {
+                            dr[j - ColumnStart] = Convert.ToString(range.Cells[i, j].Value2);
+                        }
+                        else
+                        {
+                            dr[j - ColumnStart] = DateTime.FromOADate(Convert.ToDouble(range.Cells[i, j].Value2)).ToString("yyyy-MM-dd");
+                        }
+                    }
+                    // on the first iteration we add the column headers
+                    dataTable.Rows.InsertAt(dr, dataTable.Rows.Count + 1);
+                }
+                //now save the workbook and exit Excel
+                excelworkBook.Close();
+                excel.Quit();
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                excelSheet = null;
+                range = null;
+                excelworkBook = null;
+            }
+
+        }
+        public bool WriteDataTableToExcel(System.Data.DataTable dataTable, string worksheetName, string saveAsLocation)
+        {
+            Microsoft.Office.Interop.Excel.Application excel;
+            Microsoft.Office.Interop.Excel.Workbook excelworkBook;
+            Microsoft.Office.Interop.Excel.Worksheet excelSheet;
+            Microsoft.Office.Interop.Excel.Range excelCellrange;
+
+            try
+            {
+                // Start Excel and get Application object.
+                excel = new Microsoft.Office.Interop.Excel.Application();
+
+                // for making Excel visible
+                excel.Visible = false;
+                excel.DisplayAlerts = false;
+
+                // Creation a new Workbook
+                excelworkBook = excel.Workbooks.Add(Type.Missing);
+
+                // Workk sheet
+                excelSheet = (Microsoft.Office.Interop.Excel.Worksheet)excelworkBook.ActiveSheet;
+                excelSheet.Name = worksheetName;
+
+
+                // loop through each row and add values to our sheet
+                int rowcount = 1;
+
+                foreach (DataRow datarow in dataTable.Rows)
+                {
+                    rowcount += 1;
+                    for (int i = 1; i <= dataTable.Columns.Count; i++)
+                    {
+                        // on the first iteration we add the column headers
+                        if (rowcount == 3)
+                        {
+                            excelSheet.Cells[2, i] = dataTable.Columns[i - 1].ColumnName;
+                            excelSheet.Cells.Font.Color = System.Drawing.Color.Black;
+
+                        }
+
+                        excelSheet.Cells[rowcount, i] = datarow[i - 1].ToString();
+
+                        //for alternate rows
+                        if (rowcount > 2)
+                        {
+                            if (i == dataTable.Columns.Count)
+                            {
+                                if (rowcount % 2 == 0)
+                                {
+                                    excelCellrange = excelSheet.Range[excelSheet.Cells[rowcount, 1], excelSheet.Cells[rowcount, dataTable.Columns.Count]];
+                                    FormattingExcelCells(excelCellrange, "#CCCCFF", System.Drawing.Color.Black, false);
+                                }
+
+                            }
+                        }
+
+                    }
+
+                }
+
+                // now we resize the columns
+                excelCellrange = excelSheet.Range[excelSheet.Cells[1, 1], excelSheet.Cells[rowcount, dataTable.Columns.Count]];
+                excelCellrange.EntireColumn.AutoFit();
+                Microsoft.Office.Interop.Excel.Borders border = excelCellrange.Borders;
+                border.LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+                border.Weight = 2d;
+
+
+                excelCellrange = excelSheet.Range[excelSheet.Cells[1, 1], excelSheet.Cells[2, dataTable.Columns.Count]];
+                FormattingExcelCells(excelCellrange, "#000099", System.Drawing.Color.White, true);
+
+
+                //now save the workbook and exit Excel
+
+
+                excelworkBook.SaveAs(saveAsLocation); ;
+                excelworkBook.Close();
+                excel.Quit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                excelSheet = null;
+                excelCellrange = null;
+                excelworkBook = null;
+            }
+
+        }
+
+        public bool WriteDataTableToUpdateExcel(System.Data.DataTable dataTable, string worksheetName, string saveAsLocation)
+        {
+            Microsoft.Office.Interop.Excel.Application excel;
+            Microsoft.Office.Interop.Excel.Workbook excelworkBook;
+            Microsoft.Office.Interop.Excel.Worksheet excelSheet;
+
+            try
+            {
+                // Start Excel and get Application object.
+                excel = new Microsoft.Office.Interop.Excel.Application();
+
+                // for making Excel visible
+                excel.Visible = false;
+                excel.DisplayAlerts = false;
+
+                // Creation a new Workbook
+                excelworkBook = excel.Workbooks.Open(saveAsLocation);
+
+                // Workk sheet
+                excelSheet = (Microsoft.Office.Interop.Excel.Worksheet)excelworkBook.Worksheets.Item[worksheetName];
+                // loop through each row and add values to our sheet,exclude head columns
+                int rowcount = 2;
+                foreach (DataRow datarow in dataTable.Rows)
+                {
+                    for (int i = 1; i <= dataTable.Columns.Count; i++)
+                    {
+                        excelSheet.Cells[rowcount, i].value2 = datarow[i - 1].ToString();
+                    }
+                    rowcount = rowcount + 1;
+                }
+                //now save the workbook and exit Excel
+                excelworkBook.Save();
+                excelworkBook.Close();
+                excel.Quit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                excelSheet = null;
+                excelworkBook = null;
+            }
+
+        }
+
+        public void FormattingExcelCells(Microsoft.Office.Interop.Excel.Range range, string HTMLcolorCode, System.Drawing.Color fontColor, bool IsFontbool)
+        {
+            range.Interior.Color = System.Drawing.ColorTranslator.FromHtml(HTMLcolorCode);
+            range.Font.Color = System.Drawing.ColorTranslator.ToOle(fontColor);
+            if (IsFontbool == true)
+            {
+                range.Font.Bold = IsFontbool;
+            }
+        }
+    }
+}
